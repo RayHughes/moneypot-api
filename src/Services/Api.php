cb863ac65f6a0ff307dcb726c10b75e886f419a4
Ray Hughes <ray@devphilly.om>
2018-12-11 19:41:17 -0500

    feat: remove dependency for app id, separate post and get methods, add setters for auth id and app id

--- src/Services/Api.php
+++ src/Services/Api.php
@@ -8,16 +8,21 @@ use GuzzleHttp\Exception\RequestException;
 
 class Api
 {
-    CONST BASE_URI = 'https://api.moneypot.com/v2/';
-    CONST TIMEOUT = 3.0;
+    const GET_METHOD = 'GET';
+
+    const POST_METHOD = 'POST';
+
+    const BASE_URI = 'https://api.moneypot.com/v2/';
+
+    const TIMEOUT = 3.0;
 
     /** @var string $appSecret */
     private $appSecret;
 
-    /** @var int $appId */
+    /** @var int|null $appId */
     private $appId;
 
-    /** @var $int|null $authId */
+    /** @var int|null $authId */
     private $authId;
 
     /** @var Client $client */
@@ -28,11 +33,12 @@ class Api
      * @param int $appId
      * @param int|null $authId
      */
-    public function __construct(string $appSecret, int $appId, $authId = null)
+    public function __construct(string $appSecret, ?int $authId = null, ?int $appId = null)
     {
         $this->appSecret = $appSecret;
         $this->appId = $appId;
         $this->authId = $authId;
+
         $this->client = new Client([
             'base_uri' => self::BASE_URI,
             'timeout'  => self::TIMEOUT,
@@ -40,28 +46,76 @@ class Api
     }
 
     /**
+     * @param int $authId
+     * @return void
+     */
+    public function setAuthId(int $authId)
+    {
+        $this->authId = $authId;
+    }
+
+    /**
+     * @param int $appId
+     * @return void
+     */
+    public function setAppId(int $appId)
+    {
+        $this->appId = $appId;
+    }
+
+    /**
+     * @param string $route
+     * @param array $payload
+     * @return mixed
+     */
+    public function post(string $route, array $payload = [])
+    {
+        return $this->sendPayload( $route, self::POST_METHOD, $payload);
+    }
+
+    /**
+     * @param string $route
+     * @param array $payload
+     * @return mixed
+     */
+    public function get(string $route, array $payload = [])
+    {
+        return $this->sendPayload( $route, self::GET_METHOD, $payload);
+    }
+
+    /**
      * @param string $route
      * @param string $method
      * @param array $payload
-     * @return mixed|\Psr\Http\Message\ResponseInterface|string
+     * @return mixed
      */
-    public function sendPayload(string $route, string $method = 'GET', array $payload = [])
+    private function sendPayload(string $route, string $method, array $payload)
     {
+        $queryData = ['app_secret' => $this->appSecret];
+
+        if ($this->authId) {
+            $queryData['auth_id'] = $this->authId;
+        }
+
+        if ($this->appId) {
+            $queryData['app_id'] = $this->appId;
+        }
+
         try {
             $response = $this->client->request($method, $route, [
-                'query' => [
-                    'app_id' => $this->appId,
-                    'app_secret' => $this->appSecret,
-                    'auth_id' => $this->authId,
-                ],
+                'query' => $queryData,
                 'json' => $payload,
             ]);
 
-            return json_decode($response->getBody()->getContents());
+            return json_decode($response->getBody()->getContents(), true);
         } catch(BadResponseException $e) {
             $response = $e->getResponse();
 
-            return json_decode($response->getBody()->getContents());
+            if (is_null($response)) {
+                return $e->getMessage();
+            };
+
+            return json_decode($response->getBody()->getContents(), true);
         } catch (RequestException $e) {
             return $e->getMessage();
         }
