676fb75592a82fb0cd5c604370fec740dc526bca
Ray Hughes <ray@devphilly.om>
2018-12-11 23:47:27 -0500

    feat: add exceptions for bet routes

---
+++ src/Exceptions/BetsInvalidPayoutsException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MoneyPot\Exceptions;
+
+class BetsInvalidPayoutsException extends \Exception
+{
+
+}
---
+++ src/Exceptions/BetsInvalidPropertiesException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MoneyPot\Exceptions;
+
+class BetsInvalidPropertiesException extends \Exception
+{
+
+}
